0504d964bfdda128b2a5b79967d3a3935fd4fa6b
subata <subrata.dhal@gmail.com>
2017-11-17 17:42:57 +0530

    modifications

--- admin/src/app/continent/continentdetails.component.ts
+++ admin/src/app/continent/continentdetails.component.ts
@@ -2,6 +2,7 @@ import { Component, ViewEncapsulation, NgModule, Pipe, OnInit } from '@angular/c
 import { Router, ActivatedRoute, Params} from '@angular/router';
 import { DataService } from './../data.service';
 import { Observable } from 'rxjs/Observable';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'continentdetails',
@@ -9,30 +10,57 @@ import { Observable } from 'rxjs/Observable';
 })
 
 export class ContinentdetailsComponent implements OnInit{
+  rForm: FormGroup;
+  post:any;                     // A property for our submitted form
+  description:string = '';
+  name:string = '';
+  titleAlert:string = 'This field is required';
+
   continents: any[];
   continentsID:number;
-  constructor(private service:DataService, private route: ActivatedRoute, private router: Router) {
+
+  constructor(private service:DataService, private route: ActivatedRoute, private router: Router, private fb: FormBuilder) {
+    this.rForm = fb.group({
+      'name' : [null, Validators.required],
+      'description' : [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      'validate' : ''
+    });
   }
   ngOnInit() {
-  	  //Get Users
-      this.route.params.subscribe((params: Params) => {
-        this.continentsID = params['id']; 
+
+    this.rForm.get('validate').valueChanges.subscribe(
+
+      (validate) => {
+
+          if (validate == '1') {
+              this.rForm.get('name').setValidators([Validators.required, Validators.minLength(3)]);
+              this.titleAlert = 'You need to specify at least 3 characters';
+          } else {
+              this.rForm.get('name').setValidators(Validators.required);
+          }
+          this.rForm.get('name').updateValueAndValidity();
+
       });
-       this.service.getSingleUser(this.continentsID)
-           //.subscribe(res => this.users = res );
-       .subscribe(res => {
-           this.continents = res;
-           this.continents = Array.of(this.continents); 
-           console.log(this.continents);
-       },
-       error => {
-          console.log(error);
-       }
-    );
-      
-      
+  	  //Get Users
+    //   this.route.params.subscribe((params: Params) => {
+    //     this.continentsID = params['id']; 
+    //   });
+    //    this.service.getSingleUser(this.continentsID)
+    //        //.subscribe(res => this.users = res );
+    //    .subscribe(res => {
+    //        this.continents = res;
+    //        this.continents = Array.of(this.continents); 
+    //        console.log(this.continents);
+    //    },
+    //    error => {
+    //       console.log(error);
+    //    }
+    // );
   }
- 
+  addPost(post) {
+    this.description = post.description;
+    this.name = post.name;
+  }
 
 
 }
